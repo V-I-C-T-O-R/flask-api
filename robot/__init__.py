c6780705cd19630cb9937e66d7bda9922faa96ad
victor <victor_destiny@163.com>
2019-09-03 14:59:08 +0800

    update

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import logging
+from datetime import timedelta
+from logging.handlers import TimedRotatingFileHandler
+from flask_jwt import JWT
+from robot.auth.secutiry import authenticate, identity
 
 __author__='victor'
 
@@ -7,5 +12,38 @@ def get_app():
     app.config["SECRET_KEY"] = "hc2018happynewyear"
     # app.response_class = Response
     return app
+
+def auth_access(app,config=None):
+    # jwt访问验证
+    jwt = JWT(app, authenticate, identity)  # http://localhost:5000/auth
+    if config is None:
+        return
+    app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=config.JTW_EXPIRE_TIME)
+
+def init_logger(config):
+    # 日志系统配置
+    logging.basicConfig(level=logging.DEBUG, format=config.FLASK_LOGGER_FORMAT, datefmt=config.FLASK_LOGGER_DATE_FORMAT)
+    handler = TimedRotatingFileHandler(filename=config.FLASK_LOGGER_FILE+'info.log', when="D", interval=1, backupCount=7,encoding=config.FLASK_LOGGER_CHARSET)
+
+    formatter = logging.Formatter(config.FLASK_LOGGER_FORMAT)
+    handler.setFormatter(formatter)
+    handler.setLevel(config.FLASK_LOGGER_LEVEL)
+
+    app.logger.handlers.clear()
+    app.logger.addHandler(handler)
+
+def init_gunicorn_logger(config):
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger.setLevel(logging.DEBUG)
+    gunicorn_logger.handlers.clear()
+
+    handler = TimedRotatingFileHandler(filename=config.FLASK_LOGGER_FILE + 'info.log', when="D", interval=1,
+                                       backupCount=5, encoding=config.FLASK_LOGGER_CHARSET)
+
+    formatter = logging.Formatter(config.FLASK_LOGGER_FORMAT)
+    handler.setFormatter(formatter)
+    handler.setLevel(config.FLASK_LOGGER_LEVEL)
+    gunicorn_logger.addHandler(handler)
+    return gunicorn_logger
 
 app = get_app()
--- run.py
+++ run.py
@@ -1,48 +1,43 @@
-import logging
-from datetime import timedelta
-from logging.handlers import TimedRotatingFileHandler
-
-from flask_jwt import JWT
+from setting import TestConfig,ProdConfig,DevConfig
 import setting
-from robot.auth.secutiry import authenticate, identity
-from robot import app
-
-
-def auth_access(app,config=None):
-    # jwt访问验证
-    jwt = JWT(app, authenticate, identity)  # http://localhost:5000/auth
-    if config is None:
-        return
-    app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=config.JTW_EXPIRE_TIME)
-
-def init_logger(config):
-    # 日志系统配置
-    logging.basicConfig(level=config.FLASK_LOGGER_LEVEL, format=config.FLASK_LOGGER_FORMAT, datefmt=config.FLASK_LOGGER_DATE_FORMAT)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(config.FLASK_LOGGER_LEVEL)
-    handler = TimedRotatingFileHandler(filename=config.FLASK_LOGGER_FILE+'info.log', when="D", interval=1, backupCount=7,encoding=config.FLASK_LOGGER_CHARSET)
-
-    formatter = logging.Formatter(config.FLASK_LOGGER_FORMAT)
-    handler.setFormatter(formatter)
-    handler.setLevel(config.FLASK_LOGGER_LEVEL)
-    logger.handlers.clear()
-    logger.addHandler(handler)
-    app.logger.handlers = logger.handlers
-    # return logger
+from robot import app, init_logger, auth_access, init_gunicorn_logger
 
 if __name__ == '__main__':
-    config = setting.ProdConfig
+    import sys,os
+    if len(sys.argv) > 2:
+        print('参数个数不正确,请检查')
+        sys.exit()
+    elif len(sys.argv) == 1:
+        config = TestConfig
+    elif sys.argv[1] == 'dev':
+        config = DevConfig
+    elif sys.argv[1] == 'prod':
+        config = ProdConfig
+    else:
+        config = TestConfig
+
     init_logger(config)
+    app.cfg = config
     app.logger.info('app load jwt module')
     auth_access(app,config)
     app.logger.info('app load route api module')
     from robot import api
     app.logger.info('app running at %s:%s ,env=%s ,debug=%s'%(config.FLASK_HOST,config.FLASK_PORT,config.FLASK_ENV,config.FLASK_DEBUG))
+    #可设置use_reloader=False,目的是为了只加载一次代码
     app.run(debug=config.FLASK_DEBUG, host=config.FLASK_HOST,port=config.FLASK_PORT)
 else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    '''
+        线上注意转换成ProConfig
+    '''
+    # config = TestConfig
+    config = ProdConfig
+
+    gunicorn_logger = init_gunicorn_logger(config)
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    config = setting.ProdConfig
-    auth_access(app,config)
+
+    app.logger.info('app load jwt module')
+    auth_access(app, config)
+    app.cfg = config
+    app.logger.info('app load route api module')
     from robot import api
